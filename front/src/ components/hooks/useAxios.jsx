afc245cf1421163f134f3f85ab275f00be1ff889
GyunHee <ef3232@naver.com>
2023-10-22 23:56:59 +0900

    Chore: add cross env & Feat:add local, dev script

--- front/src/ components/hooks/useAxios.jsx
+++ front/src/ components/hooks/useAxios.jsx
@@ -1,32 +1,37 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import config from '../../common/config';
 
-axios.defaults.baseURL = 'http://1.246.104.170:8080';
+axios.defaults.baseURL =
+  process.env.REACT_APP_NODE_ENV === 'development'
+    ? config.development.apiUrl
+    : process.env.REACT_APP_NODE_ENV === 'local'
+    ? config.local.apiUrl
+    : '';
 
 function useAxios(params) {
   const [response, setResponse] = useState(null);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(true);
 
-    const fetchData = async (params) => {
-      try{  
-        const res = await axios({
-          ...params,
-        });
-        setResponse(res.data);
-        }catch(err) {
-          setError(err);
-        }
-        finally{
-          setLoading(false);
-        };
-      };
+  const fetchData = async (params) => {
+    try {
+      const res = await axios({
+        ...params,
+      });
+      setResponse(res.data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchData(params);
   }, []);
 
   return { response, error, loading };
-};
+}
 
 export default useAxios;
---
+++ front/src/common/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    apiUrl: 'http://35.211.242.245:8080',
+  },
+  local: {
+    apiUrl: 'http://1.246.104.170:8080',
+  },
+};
+
+export default config;
