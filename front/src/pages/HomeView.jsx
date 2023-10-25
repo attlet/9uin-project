777339a0fea182884b9c931598a020962986d0bf
GyunHee <ef3232@naver.com>
2023-10-26 00:04:37 +0900

    Design: add CSS in pagination

--- front/src/pages/HomeView.jsx
+++ front/src/pages/HomeView.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchClipList } from '../modules/user';
 import { createAxiosInstance } from '../api/instance';
+import { styled } from 'styled-components';
 
 export default function HomeView() {
   const [postList, setPostList] = useState([]);
@@ -36,43 +37,67 @@ export default function HomeView() {
     };
 
     fetchData();
-    console.log(postList);
   }, [page]);
-
-  if (loading) return <p>Loading...</p>;
-
-  if (error) return <p>{error}</p>;
 
   const handleClick = (board_id) => {
     navigate(`/postDetail/${board_id}`);
   };
 
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
   return (
     <>
       <Logo />
       <div className={styles.projectGrid}>
-        {postList.map((post) => (
-          <div onClick={() => handleClick(post.board_id)}>
-            <Post
-              key={post.board_id}
-              title={post.title}
-              type={post.type}
-              roles={post.roles}
-              period={post.period}
-              proceed_method={post.proceed_method}
-              username={post.username}
-              tags={post.tags}
-              board_id={post.board_id}
-              view_cnt={post.view_cnt}
-              createAt={post.createAt}
-            />
-          </div>
-        ))}
+        {postList.length > 0 &&
+          postList.map((post) => (
+            <div onClick={() => handleClick(post.board_id)}>
+              <Post
+                key={post.board_id}
+                title={post.title}
+                type={post.type}
+                roles={post.roles}
+                period={post.period}
+                proceed_method={post.proceed_method}
+                username={post.username}
+                tags={post.tags}
+                board_id={post.board_id}
+                view_cnt={post.view_cnt}
+                createAt={post.createAt}
+              />
+            </div>
+          ))}
       </div>
-      <ul>
-        <li onClick={() => setPage(0)}>1</li>
-        <li onClick={() => setPage(1)}>2</li>
-      </ul>
+      <PageBox>
+        <PageIndex onClick={() => setPage(0)}>1</PageIndex>
+        <PageIndex onClick={() => setPage(1)}>2</PageIndex>
+      </PageBox>
     </>
   );
 }
+
+const PageBox = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PageIndex = styled.li`
+  list-style: none;
+  border: 1px solid #7cb2f3;
+  border-radius: 20px;
+  margin: 0.2em;
+  padding: 0.2rem 0.4rem;
+  font-size: 0.9rem;
+
+  &:hover {
+    background-color: tomato;
+    color: white;
+    border: none;
+    transform: scale(1.2);
+    transition: all 100ms ease-in-out;
+  }
+`;
