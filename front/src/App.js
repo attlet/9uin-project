02f3f8df322be42cd4bac1b34659a188e71092a6
GyunHee <ef3232@naver.com>
2023-12-02 17:17:09 +0900

    Feat: add ProtectedRoute (redirect login0

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import Home from './pages/HomeView';
 import Header from './ components/header/Header';
 import Footer from './ components/header/Footer';
 
