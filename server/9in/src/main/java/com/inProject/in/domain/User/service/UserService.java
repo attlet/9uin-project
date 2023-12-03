c9703df9d2eb3e56df6ae7c11e6443bb6c3343c4
attlet <tlsdbstjd124@naver.com>
2023-12-03 14:57:53 +0900

    Feat : add get user List
    user 리스트 가져오는 api 추가.

--- server/9in/src/main/java/com/inProject/in/domain/User/service/UserService.java
+++ server/9in/src/main/java/com/inProject/in/domain/User/service/UserService.java
@@ -9,8 +9,10 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.util.List;
+
 public interface UserService {
-    ResponseUserDto getUser(String username);
+    List<ResponseUserDto> getUserList();
     ResponseUserDto createUser(RequestUserDto userdto);
     ResponseUserDto updateUser(Long id, UpdateUserDto updateUserdto);
     void deleteUser(Long id);
